a558ea8cdca9288dd3e40f5fcf9f82ebfb48f9ae
Nudkip <42031840+Nudkip@users.noreply.github.com>
2019-12-02 05:55:23 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+class SimpleInterest {
+   constructor(p, r, t) {
+      this.principal = p;
+      this.rate = r;
+      this.time = t;
+      this.interest = p * r * t;
+   }
+}
+
+app.get('/simpleinterest', (req,res) => {
+   let obj = new SimpleInterest(req.query.p,req.query.r,req.query.t);
+
+   res.render('simpleinterest.ejs',{
+      principal : obj.principal,
+      rate : obj.rate,
+      time : obj.time,
+      interest : obj.interest
+   })
+});
+
+app.listen(app.listen(process.env.PORT || 8099));
